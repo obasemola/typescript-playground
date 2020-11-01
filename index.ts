8fd40a9af27ed5bdb93749696935032d7bdd5d24
obasemola <obasemolaayodele@gmail.com>
2020-11-01 20:43:54 +0200

    declaring types

---
+++ index.ts
@@ -0,0 +1,26 @@
+type Operation = 'multiply' | 'add' | 'divide'
+type Result = number
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch(op){
+    case 'add':
+      return a + b
+
+    case 'divide':
+      return a / b
+
+    case 'multiply':
+      return a * b
+
+    default:
+      throw new Error('Operation is not proper type')
+  }
+}
+
+try {
+  console.log(calculator(1, 3, 'divide'))
+} catch(e){
+  console.log('something went wrong, error message: ', e.message)
+}
+
+// console.log(process.argv)
